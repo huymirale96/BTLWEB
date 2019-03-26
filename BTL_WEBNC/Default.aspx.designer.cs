c3b079454739a39996f2d10e99cee995f47520a4
cuong97 <vancuong020297@gmail.com>
2019-03-26 18:50:52 +0700

    giao dien Home

--- BTL_WEBNC/Default.aspx.designer.cs
+++ BTL_WEBNC/Default.aspx.designer.cs
@@ -11,41 +11,5 @@ namespace BTL_WEBNC {
     
     
     public partial class _Default {
-        
-        /// <summary>
-        /// ListView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView ListView1;
-        
-        /// <summary>
-        /// ListView2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView ListView2;
-        
-        /// <summary>
-        /// ListView3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView ListView3;
-        
-        /// <summary>
-        /// ListView4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView ListView4;
     }
 }
