61b1b86f40cd130838e76aaaa7f0bef2694659c1
HungFFC <35162077+HungFFC@users.noreply.github.com>
2019-04-05 09:42:29 +0700

    sua login, welcome

--- BTL_WEBNC/Backend/Welcome.aspx.cs
+++ BTL_WEBNC/Backend/Welcome.aspx.cs
@@ -11,10 +11,18 @@ namespace BTL_WEBNC.Backend
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string quyen = Session["ma_quyen"].ToString();
+            string quyen = (String)(Session["ma_quyen"]);
+            string makh = (String)(Session["ma_khachang"]);
             if (String.IsNullOrEmpty(quyen))
             {
                 Response.Redirect("Login.aspx");
+            }
+            if(String.IsNullOrEmpty(makh))
+            {
+                Response.Redirect("Login.aspx");
+
+            }else{
+                Response.Redirect("Thongtincanhan.aspx");
             }
         }
     }
